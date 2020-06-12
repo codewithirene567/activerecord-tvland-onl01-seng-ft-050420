f8dc98b6469794110c0de5297224db1e5cc6bc2b
Irene Scott <irenejinscott@gmail.com>
2020-06-12 17:43:41 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    full_name = first_name, last_name
+    full_name = (first_name, last_name).to_s
   end
 end
