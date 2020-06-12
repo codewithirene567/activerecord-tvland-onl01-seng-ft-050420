ae2f0d516afe5990e259a7d1581957adaee7e603
Irene Scott <irenejinscott@gmail.com>
2020-06-12 18:14:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    list actor.collect do |actor|
+    actor.collect do |actor|
     Actor.full_name =
   end
 end
