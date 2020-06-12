cb1702329a8504ce53da7d9ac5c840b287778c3d
Irene Scott <irenejinscott@gmail.com>
2020-06-12 18:03:23 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_170417) do
+ActiveRecord::Schema.define(version: 2020_06_12_170059) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.integer "actor_id"
-  end
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.integer "actor_id"
-    t.integer "show_id"
-    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
