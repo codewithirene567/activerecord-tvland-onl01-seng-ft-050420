705b345863c4b1099170adbe0099a0788da3ee61
Irene Scott <irenejinscott@gmail.com>
2020-06-12 18:13:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
+    list = show.actor"#{self.first_name} #{self.last_name}"
     Actor.full_name
   end
 end
